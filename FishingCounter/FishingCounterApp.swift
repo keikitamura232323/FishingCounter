68553aae730c89e36202ab19ebb6f476e2ffe586
Kei Kitamura <kitamurakei@admin-kk.local>
2025-02-04 20:42:22 +0900

    最初のコミット

---
+++ FishingCounter/FishingCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingCounterApp.swift
+//  FishingCounter
+//
+//  Created by Kei Kitamura on 2025/02/02.
+//
+
+import SwiftUI
+
+@main
+struct FishingCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
